6643cf0e7c467e583bcfb80e6dcf4275c8612c3e
Arifuzzaman Moin <arifuzzamanmoin@gmail.com>
2023-06-23 01:50:07 +0800

    Designed homepage

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-06-22 09:55:15 UTC
+#Software: Joomla! 4.3.2 Stable [ Bora ] 30-May-2023 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-06-22T09:55:15+00:00	INFO ::1	joomlafailure	Empty password not allowed.
